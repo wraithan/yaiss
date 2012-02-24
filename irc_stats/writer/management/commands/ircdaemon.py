16b9650c2192b5d1f4e7b2e4e0ce9cce34cd2185
Chris McDonald <XWraithanX@gmail.com>
2012-02-24 03:14:02 -0800

    non-executed first pass at the persistence daemon

---
+++ irc_stats/writer/management/commands/ircdaemon.py
@@ -0,0 +1,25 @@
+from json import loads
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from redis import StrictRedis
+
+from writer.models import Line, Nick, Channel, Server
+from writer import LINE_ACTION_TYPE_MESSAGE
+
+class Command(BaseCommand):
+    args = ''
+    help = 'The daemon to run along side the stats viewer.'
+
+    def handle(self, *args, **options):
+        self.redis = StrictRedis(host='localhost', port=6379, db=0)
+        self.redis.subscribe('in')
+        for msg in self.redis.listen():
+            message = loads(msg['data'])
+            if message['version'] == 1:
+                if message['type'] == 'privmsg':
+                    Line.objects.create(time=datetime.now(),
+                                        message=message['data']['message']
+                                        nick=Nick.objects.get_or_create(name=message['data']['sender'], server=Server.objects.get_or_create(name='irc.freenode.net'))
+                                        channel=Channel.objects.get_or_create(name=message['data']['channel'], server=Server.objects.get_or_create(name='irc.freenode.net'))
+                                        action_type=LINE_ACTION_TYPE_MESSAGE)
